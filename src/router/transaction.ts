5cebe13f3661ece73b40722a947898735e12feec
anitabackup41122-hub <anitabackup41122@gmail.com>
2026-02-09 00:57:29 +0700

    Update transaction.ts

--- src/router/transaction.ts
+++ src/router/transaction.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import uploud from "../middleware/uploud";
 import authenticate from "../middleware/auth";
-import { createTransaction, getTransactionById, getTransactions, updateTransaction } from "../controller/transaction";
+import { createCategory, getCategories, getCategoryById, updateCategory, deleteCategory } from "../controller/category";
 
 const router = Router();
-const middlewareUpload = uploud("data/transaction");
 
-router.post("/", middlewareUpload.single("paymentProof"), createTransaction);
-router.get("/", authenticate, getTransactions);
-router.get("/:id", getTransactionById);
-router.put("/:id", authenticate, updateTransaction);
+const middlewareUpload = uploud("data/category")
+
+router.post("/checkout", middlewareUpload.single("image"), authenticate, createCategory);
+router.get("/", getCategories);
+router.get("/:id", getCategoryById);
+router.put("/:id", middlewareUpload.single("image"), authenticate, updateCategory);
+router.delete("/:id", authenticate, deleteCategory);
 
 export default router;
